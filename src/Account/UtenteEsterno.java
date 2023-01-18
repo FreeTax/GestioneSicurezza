d6e3f9a81688cc76bc922b70e6430332d063af40
Marco <trambusti.marco23@gmail.com>
2023-01-18 23:42:43 +0100

    ADD: GetUtenteInterno, GetUtenteEsterno, GetCFUSostenuti, setCfuSostenuti

--- src/Account/UtenteEsterno.java
+++ src/Account/UtenteEsterno.java
@@ -5,6 +5,7 @@ import Visite.SchedaVisita;
 import java.sql.Date;
 import java.sql.SQLException;
 import java.time.LocalDate;
+import java.util.ArrayList;
 
 public class UtenteEsterno extends Utente{
     private int idEsterno;
@@ -12,6 +13,17 @@ public class UtenteEsterno extends Utente{
     public UtenteEsterno(int codice, String nome, String cognome, String sesso, String dipartimento, Date dataNascita, int idEsterno/*, SchedaVisita visite*/) throws SQLException {
         super(codice, nome, cognome, sesso, dipartimento, dataNascita/*,visite*/);
         this.idEsterno = idEsterno;
+    }
+
+    public UtenteEsterno(int idesterno) throws SQLException {
+       ArrayList<String> resultset=uGateway.GetUtenteEsterno(idesterno);
+       codice=Integer.parseInt(resultset.get(0));
+       nome=resultset.get(1);
+       cognome=resultset.get(2);
+       sesso=resultset.get(3);
+       dataNascita=Date.valueOf(resultset.get(4));
+       dipartimento=resultset.get(5);
+       this.idEsterno = idesterno;
     }
     public void insertUtente() throws SQLException {
         uGateway.InsertUtenteEsterno(idEsterno,nome,cognome,sesso,dataNascita,dipartimento);
